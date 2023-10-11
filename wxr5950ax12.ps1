38e54a85749b6631d90b6f0758c434ae269e7c45
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2023-10-11 09:37:16 +0900

    Update wxr5950ax12.ps1

--- wxr5950ax12.ps1
+++ wxr5950ax12.ps1
@@ -6,8 +6,8 @@ New-Item C:\openwrt\buffalo\ -ItemType Directory -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/main/192-168-1-2.ps1" -OutFile "C:\openwrt\ip\192-168-1-2.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/main/192-168-11-10.ps1" -OutFile "C:\openwrt\ip\192-168-11-10.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/main/dhcp.ps1" -OutFile "C:\openwrt\ip\dhcp.ps1"
-Invoke-WebRequest -Uri "https://downloads.openwrt.org/releases/23.05.0-rc3/targets/ipq807x/generic/openwrt-23.05.0-rc3-ipq807x-generic-buffalo_wxr-5950ax12-initramfs-uImage.itb" -OutFile "C:\openwrt\tftpdroot\WXR-5950AX12-initramfs.uImage"
-Invoke-WebRequest -Uri "https://downloads.openwrt.org/releases/23.05.0-rc3/targets/ipq807x/generic/openwrt-23.05.0-rc3-ipq807x-generic-buffalo_wxr-5950ax12-squashfs-sysupgrade.bin" -OutFile "C:\openwrt\buffalo\openwrt-23.05.0-rc3-ipq807x-generic-buffalo_wxr-5950ax12-squashfs-sysupgrade.bin"
+Invoke-WebRequest -Uri "https://downloads.openwrt.org/releases/23.05.0-rc4/targets/ipq807x/generic/openwrt-23.05.0-rc4-ipq807x-generic-buffalo_wxr-5950ax12-initramfs-uImage.itb" -OutFile "C:\openwrt\tftpdroot\WXR-5950AX12-initramfs.uImage"
+Invoke-WebRequest -Uri "https://downloads.openwrt.org/releases/23.05.0-rc4/targets/ipq807x/generic/openwrt-23.05.0-rc4-ipq807x-generic-buffalo_wxr-5950ax12-squashfs-sysupgrade.bin" -OutFile "C:\openwrt\buffalo\openwrt-23.05.0-rc4-ipq807x-generic-buffalo_wxr-5950ax12-squashfs-sysupgrade.bin"
 Invoke-WebRequest -Uri "https://www.vercot.com/~serva/download/Serva_Community_v4.6.0-21120715.zip" -OutFile "C:\openwrt\Serva.zip"
 Expand-Archive -Path C:\openwrt\Serva.zip -DestinationPath C:\openwrt\Serva\ -Force
 Remove-item C:\openwrt\Serva.zip

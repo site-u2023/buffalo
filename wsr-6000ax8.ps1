5b26b016ee9886b1443b08cabcd4f421d437f0c6
site-u2023 <140032047+site-u2023@users.noreply.github.com>
2024-05-01 12:19:54 +0900

    Update wsr-6000ax8.ps1

--- wsr-6000ax8.ps1
+++ wsr-6000ax8.ps1
@@ -6,8 +6,8 @@ New-Item C:\openwrt\buffalo\ -ItemType Directory -Force
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/main/192-168-1-2.ps1" -OutFile "C:\openwrt\ip\192-168-1-2.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/main/192-168-11-10.ps1" -OutFile "C:\openwrt\ip\192-168-11-10.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/main/dhcp.ps1" -OutFile "C:\openwrt\ip\dhcp.ps1"
-Invoke-WebRequest -Uri "https://github.com/ssyysy2021/openwrt_files/raw/main/openwrt-23.05.0-rc3_bin/buffalo_wsr-6000ax8/openwrt-23.05.0-rc3-mediatek-filogic-buffalo_wsr-6000ax8-initramfs-kernel.bin" -OutFile "C:\openwrt\tftpdroot\linux.ubi-recovery"
-Invoke-WebRequest -Uri "https://github.com/ssyysy2021/openwrt_files/raw/main/openwrt-23.05.0-rc3_bin/buffalo_wsr-6000ax8/openwrt-23.05.0-rc3-mediatek-filogic-buffalo_wsr-6000ax8-squashfs-sysupgrade.bin" -OutFile "C:\openwrt\buffalo\openwrt-23.05.0-rc3-mediatek-filogic-buffalo_wsr-6000ax8-squashfs-sysupgrade.bin"
+Invoke-WebRequest -Uri "https://github.com/ssyysy2021/openwrt_files/raw/main/openwrt-23.05.3_bin/buffalo_wsr-6000ax8/openwrt-23.05.3-mediatek-filogic-buffalo_wsr-6000ax8-initramfs-kernel.bin" -OutFile "C:\openwrt\tftpdroot\linux.ubi-recovery"
+Invoke-WebRequest -Uri "https://github.com/ssyysy2021/openwrt_files/raw/main/openwrt-23.05.3_bin/buffalo_wsr-6000ax8/openwrt-23.05.3-mediatek-filogic-buffalo_wsr-6000ax8-squashfs-sysupgrade.bin" -OutFile "C:\openwrt\buffalo\openwrt-mediatek-filogic-buffalo_wsr-6000ax8-squashfs-sysupgrade.bin"
 Invoke-WebRequest -Uri "https://www.vercot.com/~serva/download/Serva_Community_v4.6.0-21120715.zip" -OutFile "C:\openwrt\Serva.zip"
 Expand-Archive -Path C:\openwrt\Serva.zip -DestinationPath C:\openwrt\Serva\ -Force
 Remove-item C:\openwrt\Serva.zip

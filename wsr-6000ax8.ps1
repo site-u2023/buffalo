55f9186be0748a43de8c04fdde602da35639b01c
site_u <140032047+site-u2023@users.noreply.github.com>
2024-12-23 15:22:00 +0900

    Update wsr-6000ax8.ps1

--- wsr-6000ax8.ps1
+++ wsr-6000ax8.ps1
@@ -8,7 +8,19 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/mai
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/site-u2023/buffalo/main/dhcp.ps1" -OutFile "C:\openwrt\ip\dhcp.ps1"
 Invoke-WebRequest -Uri "https://github.com/ssyysy2021/openwrt_files/raw/main/openwrt-23.05.3_bin/buffalo_wsr-6000ax8/openwrt-23.05.3-mediatek-filogic-buffalo_wsr-6000ax8-initramfs-kernel.bin" -OutFile "C:\openwrt\tftpdroot\linux.ubi-recovery"
 Invoke-WebRequest -Uri "https://github.com/ssyysy2021/openwrt_files/raw/main/openwrt-23.05.3_bin/buffalo_wsr-6000ax8/openwrt-23.05.3-mediatek-filogic-buffalo_wsr-6000ax8-squashfs-sysupgrade.bin" -OutFile "C:\openwrt\buffalo\openwrt-mediatek-filogic-buffalo_wsr-6000ax8-squashfs-sysupgrade.bin"
-Invoke-WebRequest -Uri "https://www.vercot.com/~serva/download/Serva_Community_v4.6.0-21120715.zip" -OutFile "C:\openwrt\Serva.zip"
+$LINKS = Invoke-WebRequest "https://www.vercot.com/~serva/download.html" -UseBasicParsing
+$LINKS_VERSION = $LINKS.Links | Where-Object {$_.href -like "*Serva_Community_*.zip*"} | Select-Object -ExpandProperty href
+$VERSION = ($LINKS_VERSION -split '/')[-1]
+Write-Host Version to install $VERSION
+Invoke-WebRequest -Uri "https://www.vercot.com/~serva/download/$VERSION" -OutFile "C:\openwrt\Serva.zip"
+while ($true){
+    if (Test-Path -Path "C:\openwrt\Serva.zip"){
+        break
+    }
+    Write-Host "File not found. Waiting..."
+    Start-Sleep -Seconds 3
+}
+
 Expand-Archive -Path C:\openwrt\Serva.zip -DestinationPath C:\openwrt\Serva\ -Force
 Remove-item C:\openwrt\Serva.zip
 Invoke-WebRequest -Uri "https://github.com/site-u2023/buffalo/raw/main/Serva.ini" -OutFile "C:\openwrt\Serva\Serva.ini"
